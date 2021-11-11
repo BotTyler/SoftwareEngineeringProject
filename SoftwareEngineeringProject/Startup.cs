9a7ac490f09d8d10cfbec4535e992a0291dd5291
Tyler Jarboe <tyler2.jarboe@gmail.com>
2021-11-10 20:34:39 -0500

    Changed the home page and added a signup page

--- SoftwareEngineeringProject/Startup.cs
+++ SoftwareEngineeringProject/Startup.cs
@@ -23,6 +23,10 @@ namespace SoftwareEngineeringProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AddPageRoute("/HomePage", "");
+            });
             services.AddRazorPages();
         }
 
