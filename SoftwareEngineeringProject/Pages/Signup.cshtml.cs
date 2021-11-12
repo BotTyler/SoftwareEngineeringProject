27e033240a6f89c62e3a2c04057e6979b277b8ee
Tyler Jarboe <tyler2.jarboe@gmail.com>
2021-11-12 00:51:20 -0500

    Starting Database

--- SoftwareEngineeringProject/Pages/Signup.cshtml.cs
+++ SoftwareEngineeringProject/Pages/Signup.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SoftwareEngineeringProject.Pages.Database;
+using SoftwareEngineeringProject.Pages.HelperClasses;
 
 namespace SoftwareEngineeringProject.Pages
 {
     public class SignupModel : PageModel
     {
+        private readonly DatabaseContext db;
+        public SignupModel(DatabaseContext db)
+        {
+            this.db = db;
+        }
+
         public void OnGet()
         {
         }
+
+        public void OnPostSubmit(SignupHelper user)
+        {
+            string temp = "Username: " + user.UName + "\nPassword: " + user.PWord;
+
+
+        }
+        /*        [HttpPost]
+                [ValidateAntiForgeryToken]
+                public IActionResult Create(UserInfo user)
+                {
+                    IEnumerable<UserInfo> objList = db.UserInfo;
+                    Console.WriteLine("OBJLIST: " + user);
+                    return Page();
+                }*/
     }
 }
--- SoftwareEngineeringProject/Startup.cs
+++ SoftwareEngineeringProject/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SoftwareEngineeringProject.Pages.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace SoftwareEngineeringProject
 {
@@ -23,6 +25,10 @@ namespace SoftwareEngineeringProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")
+                ));
             services.AddMvc().AddRazorPagesOptions(options =>
             {
                 options.Conventions.AddPageRoute("/HomePage", "");
