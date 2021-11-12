7abae44214e5596f2c6568011651669342c63ff1
Tyler Jarboe <tyler2.jarboe@gmail.com>
2021-11-12 00:51:44 -0500

    Changes to database and helper functions

---
+++ SoftwareEngineeringProject/Pages/Database/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject.Pages.Database
+{
+    public class UserInfo
+    {
+        public UserInfo()
+        {
+
+        }
+
+        [BindProperty]
+        public int ID { get; set; }
+        [BindProperty]
+        public string UName { get; set; }
+        [BindProperty]
+        public string PWord { get; set; }
+    }
+
+}
